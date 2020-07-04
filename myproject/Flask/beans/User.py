7a9b7dcced1084e6d7a065b25c6da4f06a788ba9
sagarr93 <67555742+sagarr93@users.noreply.github.com>
2020-07-04 17:04:10 +0530

    Update User.py

--- myproject/Flask/beans/User.py
+++ myproject/Flask/beans/User.py
@@ -1,11 +1,5 @@
-from api import authn
+from controller import authn
 import json
-
-import logging
-# from service import controller
-
-logging.basicConfig(level=logging.INFO)
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 RS_URL = 'https://us-4.rightscale.com/'
 
@@ -16,12 +10,6 @@ class User1:
     def __init__(self, rs_token):
         self.rs_token = rs_token
         self.RS_URL = 'https://us-4.rightscale.com/'
-        self.bearer_token
-
-    def display_rs_token(self, rs_token):
-        logging.info("Inside display_rs_token method.")
-        print(rs_token)
-        return rs_token
 
     def authenticate_user(self, rs_token):
         access_token = authn.authn_to_RS(self.RS_URL, rs_token)
@@ -31,6 +19,3 @@ class User1:
         br_token = json.loads(my_json)['access_token']
         self.bearer_token = br_token
         return access_token
-
-    def test_method(self):
-        print(self.rs_token)
