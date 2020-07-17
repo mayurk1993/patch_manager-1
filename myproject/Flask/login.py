4fe0a065d77561faae53e33d18bdd05921d54f72
mayurk1993 <mayurk1993@gmail.com>
2020-07-17 23:21:20 +0530

    Update login.py

--- myproject/Flask/login.py
+++ myproject/Flask/login.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, url_for
 from werkzeug.utils import redirect
+
 from forms import SignUpForm, EnvironmentForm, RightScriptForm
 from controller import wfm
 from beans.User import User1
@@ -37,10 +38,12 @@ def deployment_info():
             logging.error("Invalid RS Token")
             message = "Invalid RS Token, Please Re-try."
             return redirect(url_for('error', message=message))
-
+    else:
+        return render_template('second.html', form=form1)
 
 @app.route('/apply_patch', methods=['GET', 'POST'])
 def apply_patch():
+    message=""
     form1 = EnvironmentForm()
     form2 = RightScriptForm()
     if form1.is_submitted() and form1.proceed.data:
@@ -51,54 +54,80 @@ def apply_patch():
         rel_v = result['release_version']
         service = result['service']
         bearer_token = session['bearer_token']
+        
+        if(stack=="" or environment=="" or rel_v=="" or service==""):
+            message = "Kindly verify your inputs!"
+            return redirect(url_for('error', message=message))
+        
         dep_list = wfm.get_deployment_details(environment, rel_v, service, stack, bearer_token)
+        
+        
+        
         session['dep_list'] = dep_list
         dep_list1 = []
         for i in dep_list:
             a = json.loads(i)
             dep_list1.append(a)
+
         session['dep_list1'] = dep_list1
-        if len(dep_list1) == 0:
+        
+
+
+        
+        if(len(dep_list1)==0):
             logging.error("Deployment received is empty")
             message = "No Deployments found for selected parameters."
-            return redirect(url_for('error', message=message, dep_list1=dep_list1))
+            return redirect(url_for('error', message=message))
         return render_template('fourth.html', form=form2, dep_list=dep_list1)
     return render_template('second.html', form=form1)
 
-
-@app.route('/execute_right_script', methods=['GET', 'POST'])
+@app.route('/execute_right_script', methods=['GET','POST'])
 def execute_right_script():
     form2 = RightScriptForm()
 
     if form2.is_submitted() and form2.confirm.data:
         print("inside form2 submitted")
         User1.bearer_token = session['bearer_token']
-        print(request.form.getlist('selected'))
-        print(request.form['rs_name'])
+        
+        selected_list = request.form.getlist('selected')
+        rs_name = request.form['rs_name']
+        
+        if(len(selected_list)==0 or rs_name==""):
+            message = "Kindly verify your inputs"
+            return redirect(url_for('error', message=message, dep_list1=session['dep_list1']))
+        
         my_list = wfm.execute_right_script(request.form.getlist('selected'), request.form['rs_name'],
                                            session['bearer_token'])
-        if len(my_list) == 0:
+        if(len(my_list)==0):
             logging.error("Entered Rightscript doesn't exist")
             message = "Entered Rightscript doesn't exist"
             return redirect(url_for('error', message=message, dep_list1=session['dep_list1']))
+        
+#        for response in my_list:
+#            print(response.content)
         return render_template('fourth.html', form=form2, dep_list=session['dep_list1'])
-
 
 @app.route('/error', methods=['GET', 'POST'])
 def error():
     message = request.args['message']
     form = SignUpForm()
+    form1 = EnvironmentForm()
     form2 = RightScriptForm()
-
-    if message == "No Deployments found for selected parameters.":
+    
+    if(message=="No Deployments found for selected parameters."):
         dep_list1 = session['dep_list1']
-        return render_template('fourth.html', form=form2, dep_list=dep_list1, message=message)
-    elif message == "Invalid RS Token, Please Re-try.":
+        return render_template('second.html', form=form1, message=message)
+    elif(message=="Invalid RS Token, Please Re-try."):
         return render_template('login.html', form=form, message=message)
-    elif message == "Entered Rightscript doesn't exist":
+    elif(message=="Entered Rightscript doesn't exist"):
         dep_list1 = session['dep_list1']
         return render_template('fourth.html', form=form2, dep_list=dep_list1, message=message)
-
-
+    elif(message=="Kindly verify your inputs"):
+        dep_list1 = session['dep_list1']
+        return render_template('fourth.html', form=form2, dep_list=dep_list1, message=message)
+    if(message=="Kindly verify your inputs!"):
+        return render_template('second.html', form=form1, message=message)
+    
 if __name__ == '__main__':
     app.run(debug=True)
+    
